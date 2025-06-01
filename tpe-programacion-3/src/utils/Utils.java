b943331f639190f41f75584a688fdcc18fec597d
Henry Melchior <henrymelchior028@gmail.com>
2025-05-31 21:02:13 -0300

    posible backtracking

--- tpe-programacion-3/src/utils/Utils.java
+++ tpe-programacion-3/src/utils/Utils.java
@@ -1,3 +1,19 @@
 package utils;
 public class Utils {
+    void backtrack(ArrayList<Maquina> maquinas, int target, ArrayList<Maquina> estadoActual, int sumaActual, ArrayList<Maquina> mejorSolucion) {
+        if (sumaActual == target) {
+            if (mejorSolucion.isEmpty() || estadoActual.size() < mejorSolucion.size()) {
+                mejorSolucion.clear();
+                mejorSolucion.addAll(estadoActual);
+            }
+            return;
+        }
+        if (sumaActual > target) return;
+
+        for (Maquina maquina : maquinas) {
+            estadoActual.add(maquina);
+            backtrack(maquinas, target, estadoActual, sumaActual + maquina.piezas, mejorSolucion);
+            estadoActual.remove(estadoActual.size() - 1); // Backtrack
+        }
+    }
 }
